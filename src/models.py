8e3eacb61ea500e000abd03847afc1ce7025c859
Dmitry Fomichev <torman37rus@gmail.com>
2023-11-01 15:48:53 +0300

    feat: add str and repr for model

--- src/models.py
+++ src/models.py
@@ -13,3 +13,9 @@ class AppRequest(Base):
     link: Mapped[str]
     is_done: Mapped[bool]
     created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow())
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(link={self.link}, is_done={self.is_done})"
+
+    def __repr__(self):
+        return str(self)
