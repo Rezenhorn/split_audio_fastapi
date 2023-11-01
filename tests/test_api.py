faa90b9c2ca75dbb46f717fa550f6de04b210b3d
Dmitry Fomichev <torman37rus@gmail.com>
2023-11-01 12:23:15 +0300

    fix: fix api test

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,7 @@ from src.models import AppRequest
 from conftest import async_session_maker
 
 
-async def test_split_audio_api(ac: AsyncClient, test_bucket):
+async def test_split_audio_api(ac: AsyncClient, test_bucket: str):
     with mock.patch("config.settings.bucket", new=test_bucket):
         link = "https://storage.yandexcloud.net/rezenhorn-bucket/M1F1-Alaw-AFsp.wav"
         response = await ac.post("/split_audio", json={"link": link})
@@ -16,6 +16,9 @@ async def test_split_audio_api(ac: AsyncClient, test_bucket):
         assert isinstance(response.json()["left_channel_link"], str)
         assert isinstance(response.json()["right_channel_link"], str)
         async with async_session_maker() as session:
-            query = select(AppRequest)
+            query = select(AppRequest).where(
+                AppRequest.link == link,
+                AppRequest.is_done == True,  # noqa: E712
+            )
             result = await session.execute(query)
             assert len(result.all()) == 1
